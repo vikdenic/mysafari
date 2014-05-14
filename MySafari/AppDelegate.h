061d5124d73f612cf89458e29bba29966b2f1df0
vikdenic <vik.denic@gmail.com>
2014-05-14 13:19:00 -0500

    Initial Commit

---
+++ MySafari/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MySafari
+//
+//  Created by Vik Denic on 5/14/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MySafari/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MySafari
+//
+//  Created by Vik Denic on 5/14/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
