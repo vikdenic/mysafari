8bc96965e48ee12a051bd1f6e78baacf2282e26b
vikdenic <vik.denic@gmail.com>
2014-05-14 19:17:37 -0500

    MVP and Stretches

--- MySafari/ViewController.m
+++ MySafari/ViewController.m
@@ -8,11 +8,14 @@
 
 #import "ViewController.h"
 
-@interface ViewController () <UIWebViewDelegate>
+@interface ViewController () <UIWebViewDelegate, UIAlertViewDelegate, UIScrollViewDelegate>
 @property (weak, nonatomic) IBOutlet UIWebView *myWebView;
 @property (weak, nonatomic) IBOutlet UITextField *myURLTextField;
 @property (weak, nonatomic) IBOutlet UIButton *backButton;
 @property (weak, nonatomic) IBOutlet UIButton *forwardButton;
+@property (weak, nonatomic) IBOutlet UIView *buttonView;
+@property (assign,nonatomic) CGFloat locationSnapshot;
+@property (weak, nonatomic) IBOutlet UINavigationItem *navTitle;
 
 @end
 
@@ -21,6 +24,17 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.myWebView.scrollView.delegate = self;
+//    NSString *urlString = @"http://www.mobilemakers.co";
+//    NSURL *url = [NSURL URLWithString:urlString];
+//    NSURLRequest *requestHome = [NSURLRequest requestWithURL:url];
+//    [self.myWebView loadRequest:requestHome];
+
+    [self.backButton setEnabled:NO];
+    [self.forwardButton setEnabled:NO];
+
+    self.navTitle.title = @"Hello";
+
 }
 
 -(BOOL)textFieldShouldReturn:(UITextField *)textField
@@ -28,7 +42,7 @@
 {
     NSURL *url = [NSURL URLWithString:self.myURLTextField.text];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
-    [self.myWebView loadRequest:request];
+//    [self.myWebView loadRequest:request];
 
     NSString *inputString = self.myURLTextField.text;
     NSString *outputString = [NSString stringWithFormat:@"http://%@",inputString];
@@ -43,6 +57,10 @@
     else {
         [self.myWebView loadRequest:request2];
     }
+
+
+
+
     //dismisses keyboard
     [textField restorationIdentifier];
 
@@ -75,6 +93,10 @@
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView {
 
+     NSString *currentURL = webView.request.URL.absoluteString;
+
+    self.myURLTextField.text = currentURL;
+
     if ([self.myWebView canGoBack])
     {
         [self.backButton setEnabled:YES];
@@ -91,7 +113,45 @@
         [self.forwardButton setEnabled:NO];
     }
 
+    NSString *theTitle = [webView stringByEvaluatingJavaScriptFromString:@"document.title"];
+    self.navTitle.title = theTitle;
+
+
+}
+- (IBAction)onFeatureButtonPressed:(id)sender {
+    UIAlertView *alert =[[UIAlertView alloc] init];
+    alert.title = @"Coming Soon!";
+    [alert addButtonWithTitle:@"Okay"];
+    alert.delegate = self;
+    [alert show];
+
 }
 
+
+
+-(void)scrollViewDidScroll:(UIScrollView *)scrollView{
+
+    NSLog(@"%f LOCATIONSNAPSHOT",self.locationSnapshot);
+    NSLog(@"%f CURRENTLOCATION",self.myWebView.scrollView.contentOffset.y);
+
+
+
+    if (self.locationSnapshot < self.myWebView.scrollView.contentOffset.y)
+    {
+        self.myURLTextField.hidden = YES;
+
+    }
+
+    else if(self.locationSnapshot > self.myWebView.scrollView.contentOffset.y){
+        self.myURLTextField.hidden = NO;
+
+    }
+
+}
+
+-(void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView{
+    self.locationSnapshot = self.myWebView.scrollView.contentOffset.y;
+    NSLog(@"%f WHAT AM I",self.locationSnapshot);
+}
 
 @end
